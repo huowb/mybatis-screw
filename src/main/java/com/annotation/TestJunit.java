5cfd657f3af3787e3e3765eefa0bf6884b3e689e
huowb163@163.com <huowb163@163.com>
2021-06-09 16:48:52 +0800

    基本架子架构搭建

---
+++ src/main/java/com/annotation/TestJunit.java
@@ -0,0 +1,17 @@
+package com.annotation;
+
+import com.annotation.init.Colunm;
+
+/**
+ * @author hwb
+ * @version 1.0
+ * @date 2021/6/9 0009 16:35
+ */
+public class TestJunit {
+
+
+    public class Inity {
+        @Colunm
+        private String name;
+    }
+}
